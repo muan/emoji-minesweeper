c2d3dd7a047424f91e61a1985f9438b32ba8724c
Mu-An Chiou <me@muanchiou.com>
2019-10-20 14:29:26 -0400

    Add SW

---
+++ sw.js
@@ -0,0 +1,55 @@
+var APP_PREFIX = 'EmojiMinesweeper'
+var VERSION = 'v0.0.1'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  '/emoji-minesweeper/',
+  '/emoji-minesweeper/index.html',
+  '/emoji-minesweeper/game.js',
+  '/emoji-minesweeper/twemoji.js'
+
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
